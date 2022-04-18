a2670cd657707c3f066db165f434d842917f167e
Ivan Kalinić <ivan.kalinic@microblink.com>
2022-04-18 17:04:34 +0200

    Changing socket events on backend

--- backend/socket.js
+++ backend/socket.js
@@ -1,76 +1,27 @@
-const nanoid = require("nanoid")
-
-const EVENTS = {
-  connection: "connection",
-  CLIENT: {
-    CREATE_ROOM: "CREATE_ROOM",
-    SEND_ROOM_MESSAGE: "SEND_ROOM_MESSAGE",
-    JOIN_ROOM: "JOIN_ROOM",
-  },
-  SERVER: {
-    ROOMS: "ROOMS",
-    JOINED_ROOM: "JOINED_ROOM",
-    ROOM_MESSAGE: "ROOM_MESSAGE",
-  },
-};
-
-const rooms = {};
-
 const socket = ({io}) => {
   console.log(`Sockets enabled`);
 
-  io.on(EVENTS.connection, (socket) => {
-    console.log(`User connected ${socket.id}`);
+  io.on("connection", (socket) => {
 
-    socket.emit(EVENTS.SERVER.ROOMS, rooms);
-    /*
-     * When a user creates a new room
-     */
-    socket.on(EVENTS.CLIENT.CREATE_ROOM, (roomName) => {
-      console.log(roomName);
-      // create a roomId
-      const roomId = nanoid();
-      // add a new room to the rooms object
-      rooms[roomId] = {
-        name: roomName,
-      };
+    socket.on('reqTurn', (data) => {
+        const room = JSON.parse(data).room
+        io.to(room).emit('playerTurn', data)
+    })
 
-      socket.join(roomId);
+    socket.on('create', room => {
+        socket.join(room)
+    })
 
-      // broadcast an event saying there is a new room
-      socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms);
+    socket.on('join', room => {
+        socket.join(room)
+        io.to(room).emit('opponent_joined')
+    })
 
-      // emit back to the room creator with all the rooms
-      socket.emit(EVENTS.SERVER.ROOMS, rooms);
-      // emit event back the room creator saying they have joined a room
-      socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
-    });
-
-    /*
-     * When a user sends a room message
-     */
-
-    socket.on(
-      EVENTS.CLIENT.SEND_ROOM_MESSAGE,
-      (roomId, message, username) => {
-        const date = new Date();
-
-        socket.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
-          message,
-          username,
-          time: `${date.getHours()}:${date.getMinutes()}`,
-        });
-      }
-    );
-
-    /*
-     * When a user joins a room
-     */
-    socket.on(EVENTS.CLIENT.JOIN_ROOM, (roomId) => {
-      socket.join(roomId);
-
-      socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
-    });
+    socket.on('reqRestart', (data) => {
+        const room = JSON.parse(data).room
+        io.to(room).emit('restart')
+    })
+    
   });
 }
 
